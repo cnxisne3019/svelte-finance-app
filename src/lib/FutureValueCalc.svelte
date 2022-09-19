f785db4efa770ae824a26caf5e2e82c3606c55c8
Natthawut Phurahong <natthawut.plbc@gmail.com>
2022-09-19 06:05:31 +0000

    Update 3 files

--- src/lib/FutureValueCalc.svelte
+++ src/lib/FutureValueCalc.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     let result = 0;
 
-    let initailizeFund: number = 10;
-    let investPeriod: number = 30;
-    let interestRate: number = 10;
+    let initailizeFund: number = 0;
+    let investPeriod: number = 0;
+    let interestRate: number = 0;
 
     result = fvCalculation(initailizeFund, investPeriod, interestRate);
 
@@ -14,24 +14,25 @@
     }
 
     console.log('Fund is: ', initailizeFund);
+    console.log(typeof initailizeFund);
 </script>
 
 <div class="w-full max-w-xl mx-auto flex flex-col space-y-4 ">
     
     <label class="space-y-2">
-        <div class="text-sm text-slate-600 font-semibold">เงินลงทุนเริ่มต้น</div>
+        <div class="text-sm text-slate-600 font-semibold">Initialize Fund</div>
         <input class="w-full px-4 py-2 border border-slate-100 rounded-lg" type=text bind:value={initailizeFund}>
     </label>
         
     
     <label class="space-y-2">
-        <div class="text-sm text-slate-600 font-semibold">ดอกเบี้ย</div>
+        <div class="text-sm text-slate-600 font-semibold">Interest Rate</div>
         <div class="w-full bg-white px-4 py-2 border border-slate-100 rounded-lg flex justify-between">
             <input class="w-full bg-transparent" type=text bind:value={investPeriod} min=0>
-            <div class="w-1/4 text-right font-semibold text-slate-800">% ต่อปี</div>
+            <div class="w-1/4 text-right font-semibold text-slate-800">% Yearly</div>
         </div>
         
     </label>
     
-    <p>{initailizeFund} + {investPeriod} % = {initailizeFund + (initailizeFund*(investPeriod/100))} </p>
+    <pre><b>initailizeFund variable type is:</b> {typeof +initailizeFund}</pre>
 </div>
