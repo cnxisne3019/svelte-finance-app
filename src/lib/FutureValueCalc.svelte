8f0d9a6c971b6bb064095d402e0f8da55f1c937a
cnxisne3019 <cnxisne3019@gmail.com>
2022-09-19 17:35:17 +0700

    re-arrange

--- src/lib/FutureValueCalc.svelte
+++ src/lib/FutureValueCalc.svelte
@@ -1,38 +1,74 @@
 <script lang="ts">
-    let result = 0;
+	let result: any = 0;
 
-    let initailizeFund: number = 0;
-    let investPeriod: number = 0;
-    let interestRate: number = 0;
+	let presentValue: number = 0;
+	let interestRate: number = 0;
+	let investPeriod: number = 0;
 
-    result = fvCalculation(initailizeFund, investPeriod, interestRate);
+	function fvFormula(pv: number, i: number, t: number) {
+		return pv * (1 + convertPercentage(i)) ** t;
+	}
 
-    console.log(result);
+	function convertPercentage(i: number) {
+		return i / 100;
+	}
 
-    function fvCalculation (x: number, y: number, z: number){
-        return x*y+z;
-    }
+	function Calc() {
+		result = fvFormula(presentValue, interestRate, investPeriod).toFixed(2);
+	}
 
-    console.log('Fund is: ', initailizeFund);
-    console.log(typeof initailizeFund);
+	function numberWithCommas(x: number) {
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	}
+
+	interestRate = 10000;
+
+	console.log(interestRate.toLocaleString());
 </script>
 
 <div class="w-full max-w-xl mx-auto flex flex-col space-y-4 ">
-    
-    <label class="space-y-2">
-        <div class="text-sm text-slate-600 font-semibold">Initialize Fund</div>
-        <input class="w-full px-4 py-2 border border-slate-100 rounded-lg" type=text bind:value={initailizeFund}>
-    </label>
-        
-    
-    <label class="space-y-2">
-        <div class="text-sm text-slate-600 font-semibold">Interest Rate</div>
-        <div class="w-full bg-white px-4 py-2 border border-slate-100 rounded-lg flex justify-between">
-            <input class="w-full bg-transparent" type=text bind:value={investPeriod} min=0>
-            <div class="w-1/4 text-right font-semibold text-slate-800">% Yearly</div>
-        </div>
-        
-    </label>
-    
-    <pre><b>initailizeFund variable type is:</b> {typeof +initailizeFund}</pre>
+	<div class="w-full bg-slate-200 p-4 rounded-lg">
+		<h2 class="text-sm font-semibold text-slate-500">Future Value Result</h2>
+		<div class="text-3xl">{numberWithCommas(result)}</div>
+	</div>
+
+	<label class="space-y-2">
+		<div class="text-sm text-slate-600 font-semibold">Initialize Fund</div>
+		<div class="w-full bg-white px-4 py-2 border border-slate-100 rounded-lg flex justify-between">
+			<input class="w-full bg-transparent" type="text" bind:value={presentValue} min="0" />
+			<div class="w-1/4 text-right font-semibold text-slate-800">THB</div>
+		</div>
+	</label>
+
+	<label class="space-y-2">
+		<div class="text-sm text-slate-600 font-semibold">Interest Rate</div>
+		<div class="w-full bg-white px-4 py-2 border border-slate-100 rounded-lg flex justify-between">
+			<input
+				class="w-full bg-transparent"
+				pattern="^[0-9]*$"
+				type="text"
+				data-type="number"
+				bind:value={interestRate}
+				min="0"
+				required
+			/>
+			<div class="w-1/4 text-right font-semibold text-slate-800">% Yearly</div>
+		</div>
+	</label>
+
+	<label class="space-y-2">
+		<div class="text-sm text-slate-600 font-semibold">Investment Time</div>
+		<div class="w-full bg-white px-4 py-2 border border-slate-100 rounded-lg flex justify-between">
+			<input
+				class="w-full bg-transparent outline-none"
+				type="text"
+				bind:value={investPeriod}
+				min="0"
+			/>
+			<div class="w-1/4 text-right font-semibold text-slate-800">Year</div>
+		</div>
+	</label>
+
+	<button on:click={Calc} class="w-full bg-slate-700 text-white py-4">Submit</button>
+
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 	import Calc from '$lib/FutureValueCalc.svelte';
 	import '../app.css';
 </script>
@@ -10,44 +9,5 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		<Calc />
-	</h1>
+	<Calc />
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
