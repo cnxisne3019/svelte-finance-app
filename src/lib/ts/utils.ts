dd29ca4ec0f561322662b0b97b43cec6051f4b74
cnxisne3019 <cnxisne3019@gmail.com>
2022-09-21 17:12:56 +0700

    add function

--- src/lib/ts/utils.ts
+++ src/lib/ts/utils.ts
@@ -5,3 +5,18 @@ export function numberWithCommas(i: number) {
 export function convertPercentage(i: number) {
 	return i / 100;
 }
+
+// to convert interger into string with comma thousand separator
+export function int2str(i: string) {
+	return i.toLocaleString();
+}
+
+// to convert string into interger
+export function str2num(s: string) {
+	return parseFloat(s.replace(/,/g, ""));
+}
+
+// This funtion is made for allow to type only numuric and dots in input text field
+export function allowDots(s: string){
+	return s.replace(/([^0-9+.])/g, '');
+}
